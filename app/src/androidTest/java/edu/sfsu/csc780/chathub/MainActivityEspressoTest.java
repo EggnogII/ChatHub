5b0018af1d2ea0ab8575ae5c37ee33fe5541d64e
Imran <imranbirfan@gmail.com>
2017-09-26 19:04:40 -0700

    Set up for Assignment 5

--- app/src/androidTest/java/edu/sfsu/csc780/chathub/MainActivityEspressoTest.java
+++ app/src/androidTest/java/edu/sfsu/csc780/chathub/MainActivityEspressoTest.java
@@ -16,6 +16,7 @@
 
 package edu.sfsu.csc780.chathub;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -23,7 +24,9 @@ import android.test.suitebuilder.annotation.LargeTest;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
-import edu.sfsu.csc780.chathub.ui.MainActivity;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
