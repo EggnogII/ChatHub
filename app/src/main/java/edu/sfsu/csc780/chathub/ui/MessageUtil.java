4d81bd14992de76e7b618a1838bf7a10ad792bed
Imran <imranbirfan@gmail.com>
2017-09-26 20:33:06 -0700

    Updated Viewholder and adapter

--- app/src/main/java/edu/sfsu/csc780/chathub/ui/MessageUtil.java
+++ app/src/main/java/edu/sfsu/csc780/chathub/ui/MessageUtil.java
@@ -2,28 +2,40 @@ package edu.sfsu.csc780.chathub.ui;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.net.URI;
 import java.util.Calendar;
+import java.util.IllegalFormatCodePointException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import edu.sfsu.csc780.chathub.model.ChatMessage;
 import edu.sfsu.csc780.chathub.R;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 /**
  * Created by bees on 9/19/17.
@@ -33,6 +45,7 @@ public class MessageUtil
 {
     private static final String LOG_TAG = MessageUtil.class.getSimpleName();
     public static final String MESSAGES_CHILD = "messages";
+    private static FirebaseStorage sStorage= FirebaseStorage.getInstance();
     private static DatabaseReference sFirebaseDatabaseReference =
             FirebaseDatabase.getInstance().getReference();
 
@@ -105,6 +118,47 @@ public class MessageUtil
                                 .asBitmap()
                                 .into(target);
                         }
+
+                        if (chatMessage.getImageUrl() != null)
+                        {
+                            //Set View visabilities for image message
+                            viewHolder.messageImageView.setVisibility(View.VISIBLE);
+                            viewHolder.messageTextView.setVisibility(View.GONE);
+                            //Load Image for message
+
+                        }
+                        else
+                        {
+                         //set view visibilities for text message
+                            viewHolder.messageImageView.setVisibility(View.GONE);
+                            viewHolder.messageTextView.setVisibility(View.VISIBLE);
+                        }
+
+                        //load image for message
+                        try{
+                            final StorageReference gsReference =
+                                    sStorage.getReferenceFromUrl(chatMessage.getImageUrl());
+
+                            gsReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Glide.with(activity)
+                                            .load(uri)
+                                            .into(viewHolder.messageImageView);
+                                }
+
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e(LOG TAG, "Could not load image for message", e);
+                                }
+                            });
+                        }
+                        catch (IllegalFormatCodePointException e){
+                            viewHolder.messageTextView.setText("Error loading image");
+                            Log.e(LOG_TAG, e.getMessage() + " : "+ chatMessage.getImageUrl());
+                        }
+                        
                     } //End populateViewHolder
                 }; //End Firebase Recycler Adapter creation
 
@@ -135,6 +189,7 @@ public class MessageUtil
         public TextView messageTextView;
         public TextView messengerTextView;
         public CircleImageView messengerImageView;
+        public ImageView messageImageView;
 
         public MessageViewHolder(View v)
         {
@@ -142,6 +197,7 @@ public class MessageUtil
             messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
             messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
             messengerImageView = (CircleImageView) itemView.findViewById(R.id.messengerImageView);
+            messageImageView = (ImageView) itemView.findViewById(R.id.messageImageView);
         }
 
     }
