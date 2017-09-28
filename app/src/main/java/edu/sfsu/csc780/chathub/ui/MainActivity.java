9b2466736289f61419402e690ec20cd5a056a903
Imran <imranbirfan@gmail.com>
2017-09-27 20:33:35 -0700

    Changes introduced to layout of assignment 4
    Restarted from begining. Apparently the branch I was using didn't have functional send button. Also corrected implementation of layout xml files.
    
    Modified Adapter and ViewHolder
    
    Updated UI

--- app/src/main/java/edu/sfsu/csc780/chathub/ui/MainActivity.java
+++ app/src/main/java/edu/sfsu/csc780/chathub/ui/MainActivity.java
@@ -15,10 +15,15 @@
  */
 package edu.sfsu.csc780.chathub.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +38,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,8 +47,19 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import edu.sfsu.csc780.chathub.R;
@@ -58,11 +75,14 @@ public class MainActivity extends AppCompatActivity
     private static final int REQUEST_INVITE = 1;
     public static final int MSG_LENGTH_LIMIT = 10;
     public static final String ANONYMOUS = "anonymous";
+    private static final int REQUEST_PICK_IMAGE = 1;
+    private static final double MAX_LINEAR_DIMENSION = 500.0;
     private String mUsername;
     private String mPhotoUrl;
     private SharedPreferences mSharedPreferences;
     private GoogleApiClient mGoogleApiClient;
 
+    private ImageButton mImageButton;
     private FloatingActionButton mSendButton;
     private RecyclerView mMessageRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
@@ -152,6 +172,14 @@ public class MainActivity extends AppCompatActivity
                 mMessageEditText.setText("");
             }
         });
+
+        mImageButton = (ImageButton) findViewById(R.id.shareImageButton);
+        mImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                pickImage();
+            }
+        });
     }
 
     @Override
@@ -208,5 +236,138 @@ public class MainActivity extends AppCompatActivity
         // be available.
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void createMessage(Uri uri){
+        if (uri == null) Log.e(TAG, "Could not create image with null uri");
+
+        final StorageReference imageReference = MessageUtil.getImageStorageReference(mUser, uri);
+        UploadTask uploadTask = imageReference.putFile(uri);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.e(TAG, "Failed to upload image message");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ChatMessage chatMessage = new
+                        ChatMessage(mMessageEditText.getText().toString(),
+                        mUsername,
+                        mPhotoUrl,
+                        imageReference.toString());
+                MessageUtil.send(chatMessage);
+                mMessageEditText.setText("");
+            }
+        });
+    }
+
+    private Bitmap getBitmapForUri(Uri imageUri){
+        Bitmap bitmap = null;
+        try{
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    private Bitmap scaleImage(Bitmap bitmap){
+        int originalHeight = bitmap.getHeight();
+        int originalWidth = bitmap.getWidth();
+        double scaleFactor = MAX_LINEAR_DIMENSION / (double) (originalHeight + originalWidth);
+
+        if (scaleFactor < 1.0){
+            int targetWidth = (int) Math.round(originalWidth*scaleFactor);
+            int targetHeight = (int) Math.round(originalHeight*scaleFactor);
+            return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
+        }
+        else{
+            return bitmap;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: request= " + requestCode + ", result=" + resultCode);
+
+        if (requestCode == REQUEST_PICK_IMAGE && resultCode == Activity.RESULT_OK){
+            //process selected image here
+
+            if (data != null){
+                Uri uri= data.getData();
+                Log.i(TAG, "Uri: " + uri.toString());
+                //resize if image is too big
+                Bitmap bitmap= getBitmapForUri(uri);
+                Bitmap resizedBitmap= scaleImage(bitmap);
+                if (bitmap != resizedBitmap){
+                    uri = savePhotoImage(resizedBitmap);
+                }
+                createMessage(uri);
+            }
+
+            else{
+                Log.e(TAG, "Cannot get image for uploading");
+            }
+        }
+    }
+
+    private  Uri savePhotoImage(Bitmap imageBitmap){
+        File photoFile = null;
+        try{
+            photoFile= createImageFile();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        if (photoFile == null){
+            Log.d(TAG, "Error creating media file");
+            return null;
+        }
+
+        try{
+            FileOutputStream fos = new FileOutputStream(photoFile);
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        }
+
+        catch (FileNotFoundException e){
+            Log.d(TAG, "File not found: " + e.getMessage());
+        }
+
+        catch (IOException e){
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+
+        return Uri.fromFile(photoFile);
+    }
+
+    private File createImageFile() throws IOException {
+        //Create Image File Name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+        String imageFileNamePrefix= "chathub-" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File imageFile = File.createTempFile(
+                imageFileNamePrefix,
+                ".jpg",
+                storageDir
+        );
+
+        return  imageFile;
+    }
+
+    private void pickImage(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        intent.setType("image/*");
+
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
     }
 }
