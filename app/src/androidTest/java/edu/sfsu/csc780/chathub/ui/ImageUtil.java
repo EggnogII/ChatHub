7606686b6dcf4987932ff65035cf4ef01049874a
Imran <imranbirfan@gmail.com>
2017-10-05 00:00:45 -0700

    Created Instrumented Android Test

---
+++ app/src/androidTest/java/edu/sfsu/csc780/chathub/ui/ImageUtil.java
@@ -0,0 +1,90 @@
+package edu.sfsu.csc780.chathub.ui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
+
+/**
+ * Created by bees on 10/4/17.
+ */
+
+public class ImageUtil {
+
+    public static final double MAX_LINEAR_DIMENSION = 500.0;
+    public static String IMAGE_FILE_PREFIX = "chathub-";
+
+    public static Bitmap scaleImage(Bitmap bitmap) {
+        int originalHeight = bitmap.getHeight();
+        int originalWidth = bitmap.getWidth();
+        double scaleFactor =  MAX_LINEAR_DIMENSION / (double)(originalHeight + originalWidth);
+
+        // We only want to scale down images, not scale upwards
+        if (scaleFactor < 1.0) {
+            int targetWidth = (int) Math.round(originalWidth * scaleFactor);
+            int targetHeight = (int) Math.round(originalHeight * scaleFactor);
+            return Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
+        } else {
+            return bitmap;
+        }
+    }
+
+    public static Uri savePhotoImage(Context context, Bitmap imageBitmap) {
+        File photoFile = null;
+        try {
+            photoFile = createImageFile(context);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (photoFile == null) {
+            Log.d(TAG, "Error creating media file");
+            return null;
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(photoFile);
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+        return Uri.fromFile(photoFile);
+    }
+
+    public static File createImageFile(Context context) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+        String imageFileNamePrefix = IMAGE_FILE_PREFIX + timeStamp;
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File imageFile = File.createTempFile(
+                imageFileNamePrefix,    /* prefix */
+                ".jpg",                 /* suffix */
+                storageDir              /* directory */
+        );
+        return imageFile;
+    }
+
+    public static Bitmap getBitmapForUri(Context context, Uri imageUri) {
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
